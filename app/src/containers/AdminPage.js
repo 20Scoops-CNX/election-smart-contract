91ac43105e21b56292857cf669e4a30bf79df38a
Jedsada Tiwongvorakul <jt@20scoops.net>
2019-05-08 00:46:54 +0700

    add example call method contract

--- app/src/containers/AdminPage.js
+++ app/src/containers/AdminPage.js
@@ -1,14 +1,74 @@
 import React, { Component } from 'react';
+import { Form, Input, Icon, Button } from 'antd';
 import Contract from '../services/Contract';
 
 class AdminPage extends Component {
+  state = {
+    isLoading: false,
+    name: '',
+    imageUrl: ''
+  };
+
   componentDidMount() {
     Contract.setNetwork('1234567');
     this.election = Contract.Election();
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    this.election.addCandidate(
+      this.state.name,
+      this.state.imageUrl,
+      (err, res) => {
+        console.log(res);
+      }
+    );
+  };
+
+  onNameChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  onImageUrlChange = e => {
+    this.setState({ imageUrl: e.target.value });
+  };
+
   render() {
-    return <div />;
+    return (
+      <div style={{ justifyContent: 'center', display: 'flex' }}>
+        <h2>Create Candidate</h2>
+        <Form
+          onSubmit={this.handleSubmit}
+          className="login-form"
+          style={{ width: '50%', marginTop: '30px' }}
+        >
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Name"
+              onChange={this.onNameChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Image URL"
+              onChange={this.onImageUrlChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              loading={this.state.isLoading}
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
   }
 }
 
--- app/src/containers/MainPage.js
+++ app/src/containers/MainPage.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
+import Contract from '../services/Contract';
+
+const { web3 } = window;
 
 class MainPage extends Component {
+  componentDidMount() {
+    Contract.setNetwork('1234567');
+    this.election = Contract.Election();
+
+    web3.eth.getAccounts((err, accounts) => {
+      console.log(accounts);
+    });
+
+    this.election.getTotalCandidate((err, res) => {
+      console.log(Number(res.s));
+    });
+
+    this.election.allEvents((err, response) => {
+      if (!err) {
+        if (response.event === 'NewCandidateEvent') {
+          console.log('NewCandidateEvent');
+        } else if (response.event === 'VoteCandidateEvent') {
+          console.log('VoteCandidateEvent');
+        }
+      } else {
+        // TODO: handler error
+      }
+    });
+  }
+
   render() {
     return <div>MainPage</div>;
   }
